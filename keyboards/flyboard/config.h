7dd65daf6cc353bcc63d84a5910d9729aa4eb947
Eric Lind <eric.r.lind@gmail.com>
2021-06-22 23:08:31 +0200

    Successfull first build of stm32 based keyboard

--- keyboards/flyboard/config.h
+++ keyboards/flyboard/config.h
@@ -12,39 +12,41 @@
 #define DESCRIPTION ISO_Compact
 
 /* key matrix size */
-#define MATRIX_ROWS 6
-#define MATRIX_COLS 14
+#define MATRIX_ROWS 4
+#define MATRIX_COLS 4
 
 #define SPLIT_HAND_PIN A8
 
+#define OLED_DISPLAY_128X64
+
 #define RGBLED_SPLIT \
-    { 6, 6 }
+    { 10, 10 }
 
 /* key matrix pins */
-#define MATRIX_ROW_PINS_RIGHT \
-    { B0, B1, B2, B3, B4, B5 }
-#define MATRIX_COL_PINS_RIGHT \
-    { B7, C6, C7, D0, D1, D2, D3 }
-
-// #define MATRIX_ROW_PINS_LEFT \
-//     { B0, B1, B2, B3, B4, B5 }
-// #define MATRIX_COL_PINS_LEFT \
-//     { B7, C6, C7, D0, D1, D2, D3 }
-
 #define MATRIX_ROW_PINS \
-    { B0, B1, B2, B3, B4, B5 }
+    { B0, B1, B2, B3 }
 #define MATRIX_COL_PINS \
-    { B7, C6, C7, D0, D1, D2, D3 }
+    { D0, D1, D2, D3 }
+
+#define MATRIX_ROW_PINS_RIGHT \
+    { B0, B1, B2, B3 }
+#define MATRIX_COL_PINS_RIGHT \
+    { D0, D1, D2, D3 }
+
 #define UNUSED_PINS
 
 /* COL2ROW or ROW2COL */
 #define DIODE_DIRECTION ROW2COL
 
 /* number of backlight levels */
-#define BACKLIGHT_PIN B6
-#ifdef BACKLIGHT_PIN
-#    define BACKLIGHT_LEVELS 5
-#endif
+/* #define BACKLIGHT_PIN B6 */
+/* #ifdef BACKLIGHT_PIN */
+#define LED_DRIVER_ADDR_1 0b1110100
+#define LED_DRIVER_COUNT 1
+#define LED_DRIVER_1_LED_TOTAL 16
+#define DRIVER_LED_TOTAL (LED_DRIVER_1_LED_TOTAL)
+#define BACKLIGHT_LEVELS 5
+/* #endif */
 
 /* Set 0 if debouncing isn't needed */
 #define DEBOUNCING_DELAY 5
@@ -61,16 +63,39 @@
 /* prevent stuck modifiers */
 #define PREVENT_STUCK_MODIFIERS
 
-#define RGB_DI_PIN F5
+#define RGB_DI_PIN B15
 #ifdef RGB_DI_PIN
 #    define RGBLIGHT_ANIMATIONS
-#    define RGBLED_NUM 16
+#    define RGBLED_NUM 20
 #    define RGBLIGHT_HUE_STEP 8
 #    define RGBLIGHT_SAT_STEP 8
 #    define RGBLIGHT_VAL_STEP 8
-
 #endif
 
+#define WS2812_TRST_US 80
+#define WS2812_SPI SPID2            // default: SPID1
+#define WS2812_SPI_MOSI_PAL_MODE 5  // MOSI pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 5
+#define WS2812_SPI_SCK_PIN B13      // Required for F072, may be for others -- SCK pin, see the respective datasheet for the appropriate values for your MCU. default: unspecified
+#define WS2812_SPI_SCK_PAL_MODE 5   // SCK pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 5
+#define WS2812_BYTE_ORDER WS2812_BYTE_ORDER_RGB
+
+#define SOFT_SERIAL_PIN A9  // USART TX pin
+#define SELECT_SOFT_SERIAL_SPEED \
+    2                               // or 0, 2, 3, 4, 5
+                                    //  0: about 460800 baud
+                                    //  1: about 230400 baud (default)
+                                    //  2: about 115200 baud
+                                    //  3: about 57600 baud
+                                    //  4: about 38400 baud
+                                    //  5: about 19200 baud
+#define SERIAL_USART_DRIVER SD1     // USART driver of TX pin. default: SD1
+#define SERIAL_USART_TX_PAL_MODE 7  // Pin "alternate function", see the respective datasheet for the appropriate values for your MCU. default: 7
+#define SERIAL_USART_TIMEOUT \
+    100  // USART driver timeout. default 100
+         // #define SERIAL_USART_TX_PIN A9
+         // #define SERIAL_USART_RX_PIN A10
+
+/*
 #ifdef PS2_USE_USART
 #    define PS2_CLOCK_PORT PORTA
 #    define PS2_CLOCK_PIN PINA
@@ -80,9 +105,10 @@
 #    define PS2_DATA_PIN PINA
 #    define PS2_DATA_DDR DDRA
 #    define PS2_DATA_BIT 3
-
+*/
 /* synchronous, odd parity, 1-bit stop, 8-bit data, sample at falling edge */
 /* set DDR of CLOCK as input to be slave */
+/*
 #    define PS2_USART_INIT()                                                                         \
         do {                                                                                         \
             PS2_CLOCK_DDR &= ~(1 << PS2_CLOCK_BIT);                                                  \
@@ -110,5 +136,6 @@
 #    define PS2_USART_ERROR (UCSR1A & ((1 << FE1) | (1 << DOR1) | (1 << UPE1)))
 #    define PS2_USART_RX_VECT USART1_RX_vect
 #endif
+*/
 
 #endif
--- keyboards/flyboard/rules.mk
+++ keyboards/flyboard/rules.mk
@@ -7,15 +7,20 @@ BOOTLOADER = stm32-dfu
 # Build Options
 #   comment out to disable the options.
 #
-BOOTMAGIC_ENABLE = lite     # Virtual DIP switch configuration
+BOOTMAGIC_ENABLE = full
 MOUSEKEY_ENABLE = yes       # Mouse keys
 EXTRAKEY_ENABLE = yes       # Audio control and System control
-CONSOLE_ENABLE = no         # Console for debug
-COMMAND_ENABLE = no         # Commands for debug and configuration
+CONSOLE_ENABLE = yes         # Console for debug
+COMMAND_ENABLE = yes         # Commands for debug and configuration
 SLEEP_LED_ENABLE = yes       # Breathing sleep LED during USB suspend, do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
 NKRO_ENABLE = yes           # USB Nkey Rollover, if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
-BACKLIGHT_ENABLE = yes       # Enable keyboard backlight functionality
+LED_MATRIX_ENABLE = yes
+LED_MATRIX_DRIVER = IS31FL3731
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
+RGBLIGHT_DRIVER = WS2812
+
+OLED_DRIVER_ENABLE = yes
+
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 STENO_ENABLE = no
@@ -24,7 +29,10 @@ SPLIT_KEYBOARD = yes
 SERIAL_DRIVER = usart
 KEYBOARD_SHARED_EP = yes
 
-PS2_MOUSE_ENABLE = yes
-PS2_USE_USART = yes
+#Currently not working on STM32
+#PS2_MOUSE_ENABLE = yes
+#PS2_USE_USART = yes
+
+WS2812_DRIVER = spi
 
 OPT_DEFS += -DSTM32_DMA_REQUIRED=TRUE
