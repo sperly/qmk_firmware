6e9d75ad379b43bc0375792cad4652850de942c3
Eric Lind <eric.r.lind@gmail.com>
2021-06-26 12:24:53 +0200

    OLED working(ish) and RTC working(ish)

--- keyboards/flyboard/keymaps/default/keymap.c
+++ keyboards/flyboard/keymaps/default/keymap.c
@@ -1,19 +1,21 @@
 #include "flyboard.h"
 #include "keymap_swedish.h"
+#include "hal_rtc.h"
+#include <time.h>
 
 #define xxxxx KC_NO
 
+static uint16_t last_keycode = 0;
+
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_F13, KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, xxxxx, KC_F14, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, xxxxx, KC_F15, KC_LSFT, RGB_MOD, KC_S, KC_D, KC_F, KC_G, xxxxx, KC_F16, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_BTN1, KC_F17, KC_LCTL, KC_LGUI, KC_LALT, MO(1), KC_SPC, xxxxx, KC_LSFT),
                                                               [1] = LAYOUT(KC_F18, _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, xxxxx, KC_F19, _______, BL_INC, RGB_VAI, RGB_MOD, RGB_HUI, RGB_SAI, xxxxx, KC_F20, _______, BL_DEC, RGB_VAD, RGB_RMOD, RGB_HUD, RGB_SAD, xxxxx, KC_F21, _______, BL_TOGG, BL_BRTG, KC_CALC, _______, _______, KC_BTN1, KC_F22, _______, _______, _______, MO(0), KC_SPC, xxxxx, KC_LSFT),
-                                                              [2] = LAYOUT(_______, _______, KC_BSLS, ALGR(KC_2), ALGR(KC_3), ALGR(KC_4), ALGR(KC_5), xxxxx, _______, _______, _______, _______, _______, _______, _______, xxxxx, _______, _______, UC(0x03B1), _______, _______, _______, _______, xxxxx, _______, _______, _______, _______, _______, _______, _______, KC_BTN1, _______, _______, _______, _______, MO(0), KC_SPC, xxxxx, KC_LSFT)
+                                                              [2] = LAYOUT(_______, _______, KC_BSLS, ALGR(KC_2), ALGR(KC_3), ALGR(KC_4), ALGR(KC_5), xxxxx, _______, _______, _______, _______, _______, _______, _______, xxxxx, _______, _______, UC(0x03B1), _______, _______, _______, _______, xxxxx, _______, _______, _______, _______, _______, _______, _______, KC_BTN1, _______, _______, _______, _______, MO(0), KC_SPC, xxxxx, KC_LSFT)};
 
-};
+/*const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_F13, KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, xxxxx, xxxxx, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_BSPC, KC_F14, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, xxxxx, xxxxx, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_F15, KC_LSFT, RGB_MOD, KC_S, KC_D, KC_F, KC_G, xxxxx, xxxxx, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_F16, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_BTN1, KC_BTN2, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NUHS, xxxxx, KC_F17, KC_LCTL, KC_LGUI, KC_LALT, MO(1), KC_SPC, xxxxx, KC_LSFT, KC_RALT, xxxxx, KC_SPC, MO(2), KC_RGUI, KC_RCTL, KC_RSFT, KC_PWR),
+                                                              [1] = LAYOUT(KC_F18, _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, xxxxx, xxxxx, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_F19, _______, BL_INC, RGB_VAI, RGB_MOD, RGB_HUI, RGB_SAI, xxxxx, xxxxx, KC_VOLD, KC_VOLU, KC_UP, _______, KC_PGUP, _______, _______, KC_F20, _______, BL_DEC, RGB_VAD, RGB_RMOD, RGB_HUD, RGB_SAD, xxxxx, xxxxx, KC_MPLY, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, KC_F21, _______, BL_TOGG, BL_BRTG, KC_CALC, _______, _______, KC_BTN1, KC_BTN2, KC_MPRV, KC_MNXT, _______, KC_HOME, KC_END, KC_PLUS, xxxxx, KC_F22, _______, _______, _______, MO(0), KC_SPC, xxxxx, KC_LSFT, KC_RALT, xxxxx, KC_SPC, MO(2), _______, _______, _______, KC_SLEP),
+                                                              [2] = LAYOUT(_______, _______, KC_BSLS, ALGR(KC_2), ALGR(KC_3), ALGR(KC_4), ALGR(KC_5), xxxxx, xxxxx, UC(0x00A5), ALGR(KC_7), ALGR(KC_8), ALGR(KC_9), ALGR(KC_0), ALGR(KC_EQL), KC_DELETE, _______, _______, _______, _______, _______, _______, _______, xxxxx, xxxxx, _______, _______, _______, _______, _______, _______, ALGR(KC_RBRC), _______, _______, UC(0x03B1), _______, _______, _______, _______, xxxxx, xxxxx, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, _______, ALGR(KC_M), _______, _______, _______, KC_EQL, xxxxx, _______, _______, _______, _______, MO(0), KC_SPC, xxxxx, KC_LSFT, KC_RALT, xxxxx, KC_SPC, LT(1, KC_NO), _______, _______, _______, KC_SLEP)
 
-// const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_F13, KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, xxxxx, xxxxx, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_BSPC, KC_F14, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, xxxxx, xxxxx, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, KC_F15, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, xxxxx, xxxxx, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, KC_F16, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_BTN1, KC_BTN2, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NUHS, xxxxx, KC_F17, KC_LCTL, KC_LGUI, KC_LALT, MO(1), KC_SPC, xxxxx, KC_LSFT, KC_RALT, xxxxx, KC_SPC, MO(2), KC_RGUI, KC_RCTL, KC_RSFT, KC_PWR),
-//                                                               [1] = LAYOUT(KC_F18, _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, xxxxx, xxxxx, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, KC_F19, _______, BL_INC, RGB_VAI, RGB_MOD, RGB_HUI, RGB_SAI, xxxxx, xxxxx, KC_VOLD, KC_VOLU, KC_UP, _______, KC_PGUP, _______, _______, KC_F20, _______, BL_DEC, RGB_VAD, RGB_RMOD, RGB_HUD, RGB_SAD, xxxxx, xxxxx, KC_MPLY, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, KC_F21, _______, BL_TOGG, BL_BRTG, KC_CALC, _______, _______, KC_BTN1, KC_BTN2, KC_MPRV, KC_MNXT, _______, KC_HOME, KC_END, KC_PLUS, xxxxx, KC_F22, _______, _______, _______, MO(0), KC_SPC, xxxxx, KC_LSFT, KC_RALT, xxxxx, KC_SPC, MO(2), _______, _______, _______, KC_SLEP),
-//                                                               [2] = LAYOUT(_______, _______, KC_BSLS, ALGR(KC_2), ALGR(KC_3), ALGR(KC_4), ALGR(KC_5), xxxxx, xxxxx, UC(0x00A5), ALGR(KC_7), ALGR(KC_8), ALGR(KC_9), ALGR(KC_0), ALGR(KC_EQL), KC_DELETE, _______, _______, _______, _______, _______, _______, _______, xxxxx, xxxxx, _______, _______, _______, _______, _______, _______, ALGR(KC_RBRC), _______, _______, UC(0x03B1), _______, _______, _______, _______, xxxxx, xxxxx, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, KC_BTN1, KC_BTN2, _______, ALGR(KC_M), _______, _______, _______, KC_EQL, xxxxx, _______, _______, _______, _______, MO(0), KC_SPC, xxxxx, KC_LSFT, KC_RALT, xxxxx, KC_SPC, LT(1, KC_NO), _______, _______, _______, KC_SLEP)
-
-// };
+};*/
 
 //
 /* UC
@@ -50,30 +52,26 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {[0] = LAYOUT(KC_F1
 //     return MACRO_NONE;
 // }
 
-// void matrix_init_user(void) {}
+void board_init(void) {}
 
-// void matrix_scan_user(void) {
-//     static int i = 0;
-//     if (i++ > 10000) {
-//         print(".");
-//         i = 0;
-//     }
-//     print(".");
-// }
+void matrix_init_user(void) {}
+
+void matrix_scan_user(void) {
+    // static int i = 0;
+    // if (i++ > 10000) {
+    //     print(".");
+    //     i = 0;
+    // }
+    // print(".");
+}
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
-    // switch (keycode) {
-    //     case KC_A:
-    //         if (record->event.pressed) {
-    //             print("A pressed");
-    //         } else {
-    //             print("A released");
-    //         }
-    //         return true;  // Skip all further processing of this key
-    //     default:
-    //         print("Keycode");
-    //         return true;  // Process all other keycodes normally
-    // }
+    if (record->event.pressed == true) {
+        dprintf("Keycode: 0x%04X\r\n", keycode);
+        last_keycode = keycode;
+    } else {
+        last_keycode = 0;
+    }
     return true;
 }
 
@@ -103,6 +101,7 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
 oled_rotation_t oled_init_user(oled_rotation_t rotation) {
     oled_clear();
+    // print("Starting OLED");
     // if (!is_keyboard_master()) {
     return OLED_ROTATION_180;  // flips the display 180 degrees if offhand
     //}
@@ -112,10 +111,36 @@ oled_rotation_t oled_init_user(oled_rotation_t rotation) {
 
 void oled_task_user(void) {
     // Host Keyboard Layer Status
-    static const char helix_logo[] PROGMEM = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4, 0};
-    oled_write_P(helix_logo, false);
-    oled_set_cursor(2, 4);
+    // static const char helix_logo[] PROGMEM = {0x80, 0x81, 0x82, 0x83, 0x84, 0x85, 0x86, 0x87, 0x88, 0x89, 0x8a, 0x8b, 0x8c, 0x8d, 0x8e, 0x8f, 0x90, 0x91, 0x92, 0x93, 0x94, 0xa0, 0xa1, 0xa2, 0xa3, 0xa4, 0xa5, 0xa6, 0xa7, 0xa8, 0xa9, 0xaa, 0xab, 0xac, 0xad, 0xae, 0xaf, 0xb0, 0xb1, 0xb2, 0xb3, 0xb4, 0xc0, 0xc1, 0xc2, 0xc3, 0xc4, 0xc5, 0xc6, 0xc7, 0xc8, 0xc9, 0xca, 0xcb, 0xcc, 0xcd, 0xce, 0xcf, 0xd0, 0xd1, 0xd2, 0xd3, 0xd4, 0};
+    // oled_write_P(helix_logo, false);
+    RTCDateTime time;
+    struct tm   timp;
+    uint32_t    msec;
+    rtcGetTime(&RTCD1, &time);
+    rtcConvertDateTimeToStructTm(&time, &timp, &msec);
+    oled_set_cursor((oled_max_chars() - 17) / 2, 1);
+    char date_buff[18];
+    sprintf(date_buff, "%02d-%02d-%02d %02d:%02d:%02d", time.year + 80, time.month, time.day, timp.tm_hour, timp.tm_min, timp.tm_sec);
+    oled_write(date_buff, 0);
+
+    oled_set_cursor(1, 3);
     oled_write("FlyBoard is Alive!", 0);
+    oled_set_cursor(1, 4);
+    if (is_keyboard_left())
+        oled_write("I am a LEFTY", 0);
+    else
+        oled_write("I am a RIGHTY", 0);
+    oled_set_cursor(2, 6);
+    if (last_keycode != 0) {
+        oled_write("Keycode is: ", 0);
+        char buff[6];
+        sprintf(buff, "0x%04X", last_keycode);
+        oled_write(buff, 0);
+    } else {
+        oled_write("                  ", 0);
+    }
+
+    // oled_render();
 }
 
 // switch (get_highest_layer(layer_state)) {
--- keyboards/flyboard/rules.mk
+++ keyboards/flyboard/rules.mk
@@ -8,12 +8,12 @@ BOOTLOADER = stm32-dfu
 #   comment out to disable the options.
 #
 BOOTMAGIC_ENABLE = lite
-#MOUSEKEY_ENABLE = yes       # Mouse keys
-#EXTRAKEY_ENABLE = yes       # Audio control and System control
+MOUSEKEY_ENABLE = yes       # Mouse keys
+EXTRAKEY_ENABLE = yes      # Audio control and System control
 CONSOLE_ENABLE = yes         # Console for debug
 COMMAND_ENABLE = yes         # Commands for debug and configuration
 #SLEEP_LED_ENABLE = yes       # Breathing sleep LED during USB suspend, do not enable SLEEP_LED_ENABLE. it uses the same timer as BACKLIGHT_ENABLE
-#NKRO_ENABLE = yes           # USB Nkey Rollover, if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
+NKRO_ENABLE = yes           # USB Nkey Rollover, if this doesn't work, see here: https://github.com/tmk/tmk_keyboard/wiki/FAQ#nkro-doesnt-work
 #LED_MATRIX_ENABLE = yes
 #LED_MATRIX_DRIVER = IS31FL3731
 RGBLIGHT_ENABLE = yes        # Enable keyboard RGB underglow
@@ -24,11 +24,10 @@ OLED_DRIVER_ENABLE = yes
 BLUETOOTH_ENABLE = no       # Enable Bluetooth
 AUDIO_ENABLE = no           # Audio output
 STENO_ENABLE = no
-#SPLIT_KEYBOARD = yes
+SPLIT_KEYBOARD = no
 
-#SERIAL_DRIVER = usart
-#KEYBOARD_SHARED_EP = yes
-
+SERIAL_DRIVER = usart_duplex
+KEYBOARD_SHARED_EP = no
 UNICODE_ENABLE = yes
 
 #Currently not working on STM32
