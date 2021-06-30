0c1d1d9ff574c9dec2d56c8423618172c2cb41fd
Lind, Eric <eric.lind@volvocars.com>
2021-06-30 14:16:56 +0200

    Clean up of keymap, added Gaming Layer

--- keyboards/flyboard/keymaps/default/keymap.c
+++ keyboards/flyboard/keymaps/default/keymap.c
@@ -1,29 +1,38 @@
 #include "flyboard.h"
-#define xxxxx KC_NO
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Normal Layer */
     [0] = LAYOUT(
-        KC_F13, KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, xxxxx,                                    xxxxx, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_BSPC, 
-        KC_F14, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, xxxxx,                                    xxxxx, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, 
-        KC_F15, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, xxxxx,                                   xxxxx, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, 
-        KC_F16, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_BTN1,                                 KC_BTN2, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NUHS, xxxxx, 
-        KC_F17, KC_LCTL, KC_LGUI, KC_LALT, MO(1), KC_SPC, xxxxx, KC_LSFT,                 KC_RALT, xxxxx, KC_SPC, MO(2), KC_RGUI, KC_RCTL, KC_RSFT, KC_PWR
+        KC_F13, KC_ESC, KC_1, KC_2, KC_3, KC_4, KC_5, XXXXXXX,                                    XXXXXXX, KC_6, KC_7, KC_8, KC_9, KC_0, KC_MINS, KC_BSPC, 
+        KC_F14, KC_TAB, KC_Q, KC_W, KC_E, KC_R, KC_T, XXXXXXX,                                    XXXXXXX, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, KC_RBRC, 
+        KC_F15, KC_LSFT, KC_A, KC_S, KC_D, KC_F, KC_G, XXXXXXX,                                   XXXXXXX, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_QUOT, KC_ENT, 
+        KC_F16, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_B, KC_BTN1,                                 KC_BTN2, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_NUHS, XXXXXXX, 
+        KC_F17, KC_LCTL, KC_LGUI, KC_LALT, MO(1), KC_SPC, XXXXXXX, KC_LSFT,                MO(2), XXXXXXX, KC_SPC, TG(2), KC_RGUI, KC_RCTL, KC_RSFT, KC_MUTE
     ),
+    /* Fn Layer */
 	[1] = LAYOUT(
-        KC_F18, _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, xxxxx,                              xxxxx, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, 
-        KC_F19, _______, BL_INC, RGB_VAI, RGB_MOD, RGB_HUI, RGB_SAI, xxxxx,                     xxxxx, KC_VOLD, KC_VOLU, KC_UP, _______, KC_PGUP, _______, _______, 
-        KC_F20, _______, BL_DEC, RGB_VAD, RGB_RMOD, RGB_HUD, RGB_SAD, xxxxx,                    xxxxx, KC_MPLY, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, 
-        KC_F21, _______, BL_TOGG, BL_BRTG, KC_CALC, _______, _______, KC_BTN1,                  KC_BTN2, KC_MPRV, KC_MNXT, _______, KC_HOME, KC_END, KC_PLUS, xxxxx, 
-        KC_F22, _______, _______, _______, MO(0), KC_SPC, xxxxx, KC_LSFT,                 KC_RALT, xxxxx, KC_SPC, MO(2), _______, _______, _______, KC_SLEP
+        KC_F18, _______, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, XXXXXXX,                              XXXXXXX, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, 
+        KC_F19, _______, BL_INC, RGB_VAI, RGB_MOD, RGB_HUI, RGB_SAI, XXXXXXX,                     XXXXXXX, KC_VOLD, KC_VOLU, KC_UP, _______, KC_PGUP, _______, _______, 
+        KC_F20, _______, BL_DEC, RGB_VAD, RGB_RMOD, RGB_HUD, RGB_SAD, XXXXXXX,                    XXXXXXX, KC_MPLY, KC_LEFT, KC_DOWN, KC_RGHT, KC_PGDN, _______, _______, 
+        KC_F21, _______, BL_TOGG, BL_BRTG, KC_CALC, _______, _______, KC_MS_WH_UP,                  KC_MS_WH_LEFT, KC_MPRV, KC_MNXT, _______, KC_HOME, KC_END, KC_PLUS, XXXXXXX, 
+        KC_F22, _______, _______, _______, _______, KC_SPC, XXXXXXX, KC_MS_WH_DOWN,              KC_MS_WH_RIGHT, XXXXXXX, KC_SPC, TG(2), _______, _______, _______, LGUI_T(KC_L)
     ),
+    /* AltGr Layer */
 	[2] = LAYOUT(
-        _______, _______, KC_BSLS, ALGR(KC_2), ALGR(KC_3), ALGR(KC_4), ALGR(KC_5), xxxxx,     xxxxx, UC(0x00A5), ALGR(KC_7), ALGR(KC_8), ALGR(KC_9), ALGR(KC_0), ALGR(KC_EQL), KC_DELETE, 
-        _______, _______, _______, _______, _______, _______, _______, xxxxx,                   xxxxx, _______, _______, _______, _______, _______, _______, ALGR(KC_RBRC), 
-        _______, _______, UC(0x03B1), _______, _______, _______, _______, xxxxx,                   xxxxx, _______, _______, _______, _______, _______, _______, _______, 
-        _______, _______, _______, _______, _______, _______, _______, KC_BTN1,                 KC_BTN2, _______, ALGR(KC_M), _______, _______, _______, KC_EQL, xxxxx, 
-        _______, _______, _______, _______, MO(0), KC_SPC, xxxxx, KC_LSFT,                KC_RALT, xxxxx, KC_SPC, LT(1,KC_NO), _______, _______, _______, KC_SLEP
-        )
-
+        _______, _______, KC_BSLS, ALGR(KC_2), ALGR(KC_3), ALGR(KC_4), ALGR(KC_5), XXXXXXX,     XXXXXXX, UC(0x00A5), ALGR(KC_7), ALGR(KC_8), ALGR(KC_9), ALGR(KC_0), ALGR(KC_EQL), KC_DELETE, 
+        _______, _______, _______, _______, _______, _______, _______, XXXXXXX,                 XXXXXXX, _______, _______, _______, _______, _______, _______, ALGR(KC_RBRC), 
+        _______, _______, UC(0x03B1), _______, _______, _______, _______, XXXXXXX,              XXXXXXX, _______, _______, _______, _______, _______, _______, _______, 
+        _______, _______, _______, _______, _______, _______, _______, KC_BTN1,               KC_BTN2, _______, ALGR(KC_M), _______, _______, _______, KC_EQL, XXXXXXX, 
+        _______, _______, _______, _______, _______, KC_SPC, XXXXXXX, KC_LSFT,           _______, XXXXXXX, KC_SPC, TG(2), _______, _______, _______, KC_SLEP
+        ),
+    /* Gaming (Fn2) Layer */
+    [3] = LAYOUT(
+        KC_F1, KC_F6, KC_ESC, KC_1, KC_2, KC_3, KC_4, XXXXXXX,                              XXXXXXX, KC_5, KC_6, KC_7, KC_8, KC_9, KC_0, KC_DELT, 
+        KC_F2, KC_F7, KC_TAB, KC_Q, KC_W, KC_E, KC_R, XXXXXXX,                              XXXXXXX, KC_T, KC_Y, KC_U, KC_I, KC_O, KC_P, KC_LBRC, 
+        KC_F3, KC_F8, KC_LSFT, KC_A, KC_S, KC_D, KC_F, XXXXXXX,                             XXXXXXX, KC_G, KC_H, KC_J, KC_K, KC_L, KC_SCLN, KC_ENT, 
+        KC_F4, KC_F9, KC_NUBS, KC_Z, KC_X, KC_C, KC_V, KC_BTN1,                           KC_BTN2, KC_N, KC_M, KC_COMM, KC_DOT, KC_SLSH, KC_EQL, XXXXXXX, 
+        KC_F5, KC_F10, KC_LCTL, _______, MO(1), KC_SPC, XXXXXXX, KC_LSFT,           MO(2), XXXXXXX, KC_SPC, TG(0), _______, KC_RCTL, KC_RSFT, _______
+    )
 };
 
 //
